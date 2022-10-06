d0d54775ee08b0fd9cbe4ae23fd65fa2ce2eca7e
edvni <edvard.nivala@metropolia.fi>
2022-10-06 15:41:50 +0300

    Muokattu myllypuro.js --> mpuro.js ja lisätty contenttia.

---
+++ js/mpuro.js
@@ -0,0 +1,10 @@
+var map = L.map('map').setView([60.22358, 25.07794], 10);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+var markerMyllypuro = L.marker([60.22358, 25.07794]).addTo(map);
+
+markerMyllypuro.bindPopup("<b>Metropolia Myllypuro</b><br>Myllypuron Kampus");
