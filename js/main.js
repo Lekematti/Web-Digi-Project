53bc1660d6103230ce2d9083091b90dad0ac1525
edvni <edvard.nivala@metropolia.fi>
2022-10-06 15:13:13 +0300

    Lisätty arabia popu sivulle v.2

--- js/main.js
+++ js/main.js
@@ -8,6 +8,7 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 var markerKaramalmi = L.marker([60.22407, 24.75854]).addTo(map);
 var markerMyyrmaki = L.marker([60.26652, 24.85157]).addTo(map);
 var markerMyllypuro = L.marker([60.22358, 25.07794]).addTo(map);
+var markerArabia = L.marker([60.21005, 24.97682]).addTo(map);
 
 var circle = L.circle([60.22407, 24.75854], {
     color: 'red',
@@ -21,11 +22,6 @@ markerMyyrmaki.bindPopup("<b>Metropolia Myyrmäki</b><br>Myyrmäen Kampus");
 markerMyllypuro.bindPopup("<b>Metropolia Myllypuro</b><br>Myllypuron Kampus");
 markerArabia.bindPopup("<b>Metropolia Arabia</b><br>Arabian Kampus");
 circle.bindPopup("I am a circle.");
-
-var popup = L.popup()
-    .setLatLng([51.513, -0.09])
-    .setContent("I am a standalone popup.")
-    .openOn(map);
 
 function onMapClick(e) {
     popup
