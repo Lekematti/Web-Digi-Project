bcaa5e366f1fb52af9945be8a7111100eadf6974
edvni <edvard.nivala@metropolia.fi>
2022-10-11 14:42:23 +0300

    Lisätty säänhaulle "Loading..." -indikaattori sekä laitettu weatherin komponenteille uudet asettelut -> margin-left: 0.5em.

--- js/weather.js
+++ js/weather.js
@@ -1,6 +1,6 @@
 let weather = {
-    apiKey: "57d63e83d484177c2110ffbd1ac592c1",
-    fetchWeather: function (city) {
+    apiKey: "57d63e83d484177c2110ffbd1ac592c1",                                 // Openweathermap apiKey
+    fetchWeather: function (city) {                                             // Säähaun funktio
         fetch("https://api.openweathermap.org/data/2.5/weather?q=" +
             city +
             "&appid=" +
@@ -9,18 +9,20 @@ let weather = {
         ).then((response) => response.json())
             .then((data) => this.displayWeather(data));
     },
-    displayWeather: function (data) {
+    displayWeather: function (data) {                                // Funktio sään näyttämiselle.
         const {name} = data;
         const {icon, description} = data.weather[0];
         const {temp, humidity} = data.main;
         const {speed} = data.wind;
         console.log(name,icon,description,temp,humidity,speed);
-        document.querySelector(".city").innerText = "Weather in " + name;
-        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
-        document.querySelector(".description").innerText = description;
-        document.querySelector(".temp").innerText = temp + "°C";
-        document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
-        document.querySelector(".wind").innerText = "Wind Speed: " + speed + " m/s";
+        document.querySelector(".city").innerText = "Weather in " + name;                                   // Asettaa halutun paikan nimen.
+        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + "@2x.png";      // Asettaa openweathermapin kuvakirjastosta oikean sääkuvan.
+        document.querySelector(".description").innerText = description;                                     // Asettaa millainen keli on (clear sky, cloudy etc..).
+        document.querySelector(".temp").innerText = temp + "°C";                                            // Asettaa lämpötilan.
+        document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";                      // Asettaa kosteustason.
+        document.querySelector(".wind").innerText = "Wind Speed: " + speed + " m/s";                        // Asettaa tuulen nopeuden.
+
+        document.querySelector(".weather").classList.remove("loading");
     },
     search: function () {
         this.fetchWeather(document.querySelector(".search-bar").value);
@@ -39,13 +41,13 @@ document.querySelector(".search-bar").addEventListener("keyup", function (event)
     }
 });
 
+// Hakee eri kampusten säätiedot.
 function karamalmi () {
     weather.fetchWeather("Karamalmi");
 }
 function myyrmaki () {
     weather.fetchWeather("Myyrmäki");
 }
-// Hakee Arábian sään. EI HELSINGIN ARABIAN.
 function arabia () {
     weather.fetchWeather("Arabianranta");
 }
@@ -55,9 +57,3 @@ function myllypuro () {
 function helsinki () {
     weather.fetchWeather("Helsinki");
 }
-
-// Hakee kyseisen kaupungin säätiedot oletuksena kun sivun lataa.
-//weather.fetchWeather("Karamalmi");
-
-
-/* "https://api.openweathermap.org/data/2.5/weather?lat=60.22407&lon=24.75854&appid=57d63e83d484177c2110ffbd1ac592c1&units=metric" */
